fc59602569120fe42b0f8d5a0487f07945d5b845
ririki <nakabayashi@keylink.co.jp>
2019-10-15 10:15:31 +0900

    ログインページとホームのルーティング

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './js/pages/Home';
+import Login from './js/pages/Login';
 
 export default class App extends React.Component {
   render(){
     return (
       <BrowserRouter>
         <div>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' component={Login} />
+          <Route exact path='/Login' component={Login} />
+          <Route exact path='/Home' component={Home} />
         </div>
       </BrowserRouter>
     );
--- src/js/pages/Login.jsx
+++ src/js/pages/Login.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default class Home extends React.Component {
+export default class Login extends React.Component {
   render(){
     return (
         <div>
-            HOME
+            LOGIN
         </div>
     );
   }
